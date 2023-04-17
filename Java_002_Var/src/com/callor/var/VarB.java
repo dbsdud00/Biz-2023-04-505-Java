47bad4c10fabafaad21e54905cc9376959252fd5
dbsdud00 <dbsdudsw@icloud.com>
2023-04-17 09:48:36 +0900

    var

---
+++ Java_002_Var/src/com/callor/var/VarB.java
@@ -0,0 +1,16 @@
+package com.callor.var;
+
+public class VarB {
+	public static void main(String[] args) {
+		int num1 = 0;
+		int num2 = 0;
+		// 변수에 어떤 값을 저장, 할당, 대입하려면 반드시 먼저 선언되어야 한다.
+		num1 = 30;
+		num2 = 40;
+		
+		// 선언과 동시에 두 변수의 덧셈 결과 저장
+		int sum1 = num1 + num2;
+		
+		System.out.println(sum1);
+	}
+}
