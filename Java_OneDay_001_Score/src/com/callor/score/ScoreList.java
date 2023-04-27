b4395818a31cd6d35a023eecf0e311cb9b2abe88
dbsdud00 <dbsdudsw@icloud.com>
2023-04-27 16:29:21 +0900

    0427_Oneday_Project

--- Java_OneDay_001_Score/src/com/callor/score/ScoreList.java
+++ Java_OneDay_001_Score/src/com/callor/score/ScoreList.java
@@ -16,20 +16,20 @@ public class ScoreList {
 			scoreService.scoreSet(stds[i], num); // stds 객체 배열의 학생 점수 세팅
 		}
 
-		System.out.println("=".repeat(80));
+		System.out.println("=".repeat(75));
 		System.out.println("* 한울 고교 성적 리스트 *");
-		System.out.println("=".repeat(80));
-		System.out.println("학번\t\t 국어\t 영어\t 수학\t 음악\t 미술\t 총점\t 평균\t 등수");
-		System.out.println("-".repeat(80));
+		System.out.println("=".repeat(75));
+		System.out.println("학번\t\t 국어\t 영어\t 수학\t 음악\t 미술\t 총점\t 평균");
+		System.out.println("-".repeat(75));
 		
 		for (int i = 0; i < stds.length; i++) { // stds 배열의 학생 객체들의 정보 출력
 			scoreService.scorePrint(stds[i]);
 			System.out.println();
 		}
 		
-		System.out.println("-".repeat(80));
+		System.out.println("-".repeat(75));
 		scoreService.totalAvgPrint(stds); // 전체점수와 전체 평균 출력
-		System.out.println("=".repeat(80));
+		System.out.println("=".repeat(75));
 
 	}
 }
