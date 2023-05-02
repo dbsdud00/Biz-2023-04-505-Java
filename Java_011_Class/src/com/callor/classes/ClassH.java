2948c4f9e5830e82afd8e18a93b014de880f67a4
dbsdud00 <dbsdudsw@icloud.com>
2023-05-02 17:15:59 +0900

    0502_scan

--- Java_011_Class/src/com/callor/classes/ClassH.java
+++ Java_011_Class/src/com/callor/classes/ClassH.java
@@ -6,27 +6,51 @@ public class ClassH {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int num1 = 0;
-		int num2 = 0;
-
+		String str1;
+		String str2;
+		int num1;
+		int num2;
+		
 		while(true) {
 			System.out.println("두 개의 정수를 입력하세요.");
-			System.out.println("-1을 입력하면 종료합니다.");
+			System.out.println("정수가 아닌 수를 입력하면 종료합니다.");
 			System.out.print("정수1 >> ");
-			num1 = scan.nextInt();
+			str1 = scan.nextLine();
+			if (isInteger(str1) == true ) {
+				num1 = Integer.parseInt(str1);
+			} else {
+				break;
+			}
 
 			System.out.print("정수2 >> ");
-			num2 = scan.nextInt();
-
+			str2 = scan.nextLine();
+			if (isInteger(str2) == true ) {
+				num2 = Integer.parseInt(str1);
+			}
+			else {
+				break;
+			}
 			System.out.printf("%d + %d = %d\n", num1, num2, num1+num2);
 			System.out.printf("%d - %d = %d\n", num1, num2, num1 - num2);
 			System.out.printf("%d * %d = %d\n", num1, num2, num1 * num2);
 			System.out.printf("%d / %d = %.2f\n", num1, num2, (float) num1 / (float) num2);
 			System.out.println();
-			if ((-2 < num1 && num1 < 0) || (-2 < num2 && num2 < 0)) {
-				break;
-			}
+			
+			
+
 		}
 		System.out.println("GAME OVER!");
 	}
+	 public static boolean isInteger(String num)
+     {
+      try
+      {
+       Integer.parseInt(num);    // int 형으로 변환해보고
+       return true;                      // 이상없으면 true를 리턴
+      }
+      catch (NumberFormatException e)
+      {
+       return false;                    // 이상 있으면 false를 리턴
+      }
+     }
 }
