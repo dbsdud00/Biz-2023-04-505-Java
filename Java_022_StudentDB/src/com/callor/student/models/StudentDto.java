3a99486ac3146ef7d139dd55109ff5583a1b3465
dbsdud00 <dbsdudsw@icloud.com>
2023-05-26 13:30:20 +0900

    0526

---
+++ Java_022_StudentDB/src/com/callor/student/models/StudentDto.java
@@ -0,0 +1,41 @@
+package com.callor.student.models;
+
+public class StudentDto {
+
+	// DTO 의 이름이 DB의 칼럼 이름과 똑같아야 한다.
+	public String st_num;			//	VARCHAR2(6)
+	public String st_name;		//	nVARCHAR2(20)
+	public String st_dept;		//	nVARCHAR2(20)
+	public int st_grade;		//	NUMBER
+	public String st_tel;			//	VARCHAR2(10)
+	
+	
+	
+	public StudentDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public StudentDto(String st_num, String st_name, String st_dept, int st_grade, String st_tel) {
+		super();
+		this.st_num = st_num;
+		this.st_name = st_name;
+		this.st_dept = st_dept;
+		this.st_grade = st_grade;
+		this.st_tel = st_tel;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "StudentDto [st_num=" + st_num + ", st_name=" + st_name + ", st_dept=" + st_dept + ", st_grade="
+				+ st_grade + ", st_tel=" + st_tel + "]";
+	}
+
+	
+	
+	
+}
